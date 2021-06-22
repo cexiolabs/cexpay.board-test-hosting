8bcd191cef35ee4b5d09c67385cda61f84ea2374
Maksym Anurin <m.anurin@cexiolabs.com>
2021-06-22 23:55:22 +0300

    0.4.0

---
+++ yong/precache-manifest.9f64078ab975e340b8c363dddeab684e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9d2bc1497e2d311a78065d3fc499e8",
+    "url": "/yong/index.html"
+  },
+  {
+    "revision": "c4346bf75a883c1e0743",
+    "url": "/yong/static/css/main.930770e4.chunk.css"
+  },
+  {
+    "revision": "86f82488d70aaee9fb24",
+    "url": "/yong/static/js/2.e4676cd7.chunk.js"
+  },
+  {
+    "revision": "182d4eea77beabf818076dfb055cebc0",
+    "url": "/yong/static/js/2.e4676cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4346bf75a883c1e0743",
+    "url": "/yong/static/js/main.2b6a75ee.chunk.js"
+  },
+  {
+    "revision": "9823edbd72ec4e253752",
+    "url": "/yong/static/js/runtime-main.d4405be1.js"
+  }
+]);
